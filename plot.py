5957a3d131a30902484a9275c4b3c6d00732d2de
lpnueg4 <lpnueg@163.com>
2018-04-16 17:00:17 +0800

    自动解压wiz文件

--- plot.py
+++ plot.py
@@ -9,8 +9,14 @@ import re
 import glob
 import os
 
+import shutil
+
+import zipfile
+
+DES_FILE = 'D:\PY\plot\data.zip'
+
 def get_data():
-    fn = "index.html"
+    fn = "%s/%s" % (DES_FILE + "_files", "index.html")
 
     with open(fn, "rb") as f:
         content = f.read()
@@ -66,7 +72,7 @@ def format_data():
     return (dd, mark)
 
 
-def get_top_num_and_index(input_list, num, flag='max'):
+def get_top_num_and_index(input_list, num, flag='max', space=0):
     time = {}
     for index,i in enumerate(input_list):
         time[index] = float(i)*1000
@@ -78,6 +84,8 @@ def get_top_num_and_index(input_list, num, flag='max'):
 
     sort_list = sorted(time.items(), key=lambda item: item[1], reverse=f)
     ret_list = sort_list[:num]
+
+    # pprint(ret_list)
 
     return ret_list
 
@@ -115,7 +123,7 @@ def plot():
 
     data, make = format_data()
 
-    plt.figure(figsize=(22,10), dpi=80)
+    plt.figure(figsize=(26,10), dpi=80)
     # plt.rcParams['font.sans-serif'] = ['SimHei']
     plt.rcParams['font.serif'] = ['consola']
 
@@ -124,13 +132,23 @@ def plot():
 
     l_idol, = plt.plot(data['all'], marker='o', label="idol")
 
+    # top 5
     top_idols = get_top_num_and_index(data['all'], 5)
+
+    space_flag = 0
+
     for i in top_idols:
-        plt.text(i[0],
-            data['all'][i[0]]+1500,
-            '%.0f' % float(data['all'][i[0]]),
-            ha='center',
-            va='bottom')
+
+        # 防止2个大值靠的太近. 重叠
+        if i[0]>space_flag+3 or i[0]<space_flag-3:
+            # print data['all'][i[0]]
+            plt.text(i[0],
+                data['all'][i[0]]+1500,
+                '%.0f' % float(data['all'][i[0]]),
+                ha='center',
+                va='bottom')
+            # i[0] 是 x轴
+            space_flag = i[0]
 
     #---------------------------------------------------------
     # boss
@@ -213,14 +231,16 @@ def plot():
         '%s' % (map_data[min_map] / plus),
         ha='center',va='top', fontsize=8)
 
-
     #---------------------------------------------------------
     # X轴 make
     plt.axhline(60000, ls="--", c="r", alpha=0.2)
-    plt.text(-6.8, 60000, '60000',  color='r', fontsize=10, ha='center', va='center', alpha=0.7)
+
+    # TODO 根据数据数量控制x轴位置
+    #          x     y
+    plt.text(-7.8, 60000, '60000',  color='r', fontsize=10, ha='center', va='center', alpha=0.7)
 
     plt.axhline(40000, ls="--", c="g", alpha=0.2)
-    plt.text(-6.8, 40000, '40000',  color='g', fontsize=10, ha='center', va='center', alpha=0.7)
+    plt.text(-7.8, 40000, '40000',  color='g', fontsize=10, ha='center', va='center', alpha=0.7)
 
     # plt.axhline(20000, ls="--", c="b", alpha=0.1)
     plt.axhline(0, ls="-", c="k", linewidth=0.5)
@@ -263,7 +283,6 @@ def plot():
     for index, i in enumerate(data['idol_buff']):
         if int(i[:-1]) == 0:
             plt.plot(index, data['all'][index],'rx', markersize=8)
-
 
     #---------------------------------------------------------
     # time
@@ -310,7 +329,6 @@ def plot():
     #---------------------------------------------------------
     # text
 
-
     #---------------------------------------------------------
     # legend
 
@@ -343,7 +361,33 @@ def plot():
 
     save(plt)
 
+    print "plot over."
+
+def copy():
+    src = 'D:\Documents\My Knowledge\Data\lpnueg@163.com\Game\Crusaders of the Lost Idols\idols 计算.ziw'.decode('u8')
+    DES_FILE = 'D:\PY\plot\data.zip'
+    try:
+        shutil.copy(src, DES_FILE)
+    except Exception, e:
+        print e
+    print "copy over."
+
+def unzip():
+    zip_file = zipfile.ZipFile(DES_FILE)
+
+    if os.path.isdir(DES_FILE + "_files"):
+        pass
+    else:
+        os.mkdir(DES_FILE + "_files")
+
+    for names in zip_file.namelist():
+        zip_file.extract(names, DES_FILE + "_files")
+
+    zip_file.close()
+    print "unzip over."
 
 if __name__ == "__main__":
 
+    copy()
+    unzip()
     plot()
